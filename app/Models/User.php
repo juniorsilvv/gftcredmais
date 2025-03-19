719836773892264df31a4272e6a7df02c8a75db5
JUnior Silva <hjuniorbsilva@gmail.com>
2025-03-18 21:08:03 -0300

    feat: adicionando funções do JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject 
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +42,32 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Criptografa a senha
+     *
+     * @param [type] $value
+     * @author Junior <hjuniorbsilva@gmail.com>
+     */
+    public function setPasswordAttribute($value)
+    {
+        if(!empty($value))
+            $this->attributes['password'] = Hash::make($value);
+    }
+
+    /**
+     * Retorna a identificação do usuário para o JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Retorna um array de claims personalizadas para o JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
