6755378d5e3360d601089b1c239959d0f9e87433
JUnior Silva <hjuniorbsilva@gmail.com>
2025-03-19 20:39:15 -0300

    feat: adicioando relacionamentos

--- app/Models/LoanContracts.php
+++ app/Models/LoanContracts.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LoanContracts extends Model
 {
@@ -11,7 +12,12 @@ class LoanContracts extends Model
 
     protected $table = 'loan_contracts';
 
+    protected $keyType = 'string'; 
+    public $incrementing = false; 
+    protected $primaryKey = 'id';
+
     protected $fillable = [
+        'id',
         'client_id', 
         'amount', 
         'commercial_manager_id', 
@@ -19,4 +25,51 @@ class LoanContracts extends Model
         'superintendent_id', 
         'status'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            /**
+             * Cria a chave primaria de forma automatica caso não seja setado
+             */
+            if (empty($model->id)) {
+                $model->id = Str::uuid()->toString();
+            }
+        });
+    }
+
+    /**
+     * Retorna a relação do gerente comercial
+     *
+     * @author Junior <hamilton.junior@opovodigital.com>
+     */
+    public function commercialManager()
+    {
+        return $this->belongsTo(User::class, 'commercial_manager_id')->select('id', 'name', 'email');
+    }
+
+    /**
+     * Retorna a relação com o gerente regional
+     *
+     * @author Junior <hamilton.junior@opovodigital.com>
+     */
+    public function regionalManager()
+    {
+        return $this->belongsTo(User::class, 'regional_manager_id')->select('id', 'name', 'email');
+    }
+
+
+
+    /**
+     * Retorna a relação com o superintente
+     *
+     * @return void
+     * @author Junior <hamilton.junior@opovodigital.com>
+     */
+    public function superintendent()
+    {
+        return $this->belongsTo(User::class, 'superintendent_id')->select('id', 'name', 'email');
+    }
 }
