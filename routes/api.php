f01be8b4061dd0d6029744255801e6ed59eaab65
JUnior Silva <hjuniorbsilva@gmail.com>
2025-03-20 09:46:36 -0300

    feat: adicionado rota para logout

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function(){
     Route::post('/login', 'login');
     Route::post('/register', 'register');
-
 });
 
 Route::middleware(AuthMiddleware::class)->group(function(){
+    Route::get('logout', [AuthController::class, 'logout']);
+
     Route::controller(ContractsController::class)->prefix('contracts')->group(function(){
         Route::get('{uuid}', 'get');
         Route::get('/', 'all');
