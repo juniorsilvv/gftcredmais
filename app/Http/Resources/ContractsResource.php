b9769f41be1ddbabbcd7f30abc432c5bb4ae0947
JUnior Silva <hjuniorbsilva@gmail.com>
2025-03-20 11:07:28 -0300

    feat: retornando a informação do usuário vindo da api junto com os dados no resource

--- app/Http/Resources/ContractsResource.php
+++ app/Http/Resources/ContractsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Services\UserServices;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -12,8 +13,23 @@ class ContractsResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+    public function toArray(Request $request)
     {
-        return parent::toArray($request);
+        $user = (new UserServices)->getUser($this->client_id);
+        return [
+            'id'                    => $this->id,
+            'client_id'             => $this->client_id,
+            'amount'                => $this->amount,
+            'commercial_manager_id' => $this->commercial_manager_id,
+            'regional_manager_id'   => $this->regional_manager_id,
+            'superintendent_id'     => $this->superintendent_id,
+            'status'                => $this->status,
+            'created_at'            => $this->created_at,
+            'updated_at'            => $this->updated_at,
+            'client'                => $user['error'] ? null : $user['data'],
+            'commercialManager'     => $this->commercialManager,
+            'regionalManager'       => $this->regionalManager,
+            'superintendent'        => $this->superintendent
+        ];
     }
 }
